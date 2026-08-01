4b1cdd8607fc4255ed65cf15964d73c50bf19468
BobPlayz <its.maharshi.official@gmail.com>
2026-08-01 18:49:29 +0530

    Create database package

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { healthRouter } from "./health.js";
+import { chatRouter } from "./chat.js";
 
 export const apiRouter = Router();
 
 apiRouter.use(healthRouter);
+apiRouter.use(chatRouter);
