dd4f13b11e4f40af33abd58554bb1fa36916bbdc
BobPlayz <its.maharshi.official@gmail.com>
2026-08-27 17:15:06 +0530

    feat(auth): export email OTP schema

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,47 +1,35 @@
 export * from "./users.js";
 export * from "./workspaces.js";
 export * from "./workspaceMembers.js";
-
 export * from "./organizations.js";
 export * from "./organizationMembers.js";
-
 export * from "./conversations.js";
 export * from "./messages.js";
-
 export * from "./memories.js";
 export * from "./memoryEmbeddings.js";
-
 export * from "./projects.js";
 export * from "./projectFiles.js";
-
 export * from "./uploads.js";
 export * from "./images.js";
 export * from "./voices.js";
-
 export * from "./personalities.js";
 export * from "./settings.js";
-
 export * from "./apiKeys.js";
 export * from "./sessions.js";
-
 export * from "./tasks.js";
 export * from "./reminders.js";
-
 export * from "./tools.js";
 export * from "./toolLogs.js";
-
 export * from "./agents.js";
 export * from "./agentRuns.js";
-
 export * from "./auditLogs.js";
 export * from "./usageRecords.js";
 export * from "./notifications.js";
-
 export * from "./integrations.js";
 export * from "./modelProviders.js";
 export * from "./workflows.js";
 export * from "./searchIndex.js";
 export * from "./webhooks.js";
-
 export * from "./billingPlans.js";
 export * from "./subscriptions.js";
+export * from "./emailOtps.js";
